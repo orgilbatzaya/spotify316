85654db4fe3e2355da43e0f3910b67d0ceb80777
Orgil Batzaya <ob29@duke.edu>
2019-10-26 04:28:18 -0400

    playing with carousel and top artists api

--- src/web-api-auth-examples/authorization_code/app.js
+++ src/web-api-auth-examples/authorization_code/app.js
@@ -35,7 +35,7 @@ var generateRandomString = function(length) {
 var stateKey = 'spotify_auth_state';
 
 var app = express();
-
+console.log(__dirname);
 app.use(express.static(__dirname + '/public'))
    .use(cors())
    .use(cookieParser());
@@ -46,7 +46,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email';
+  var scope = 'user-read-private user-read-email user-top-read';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
