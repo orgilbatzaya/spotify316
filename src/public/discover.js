eaeefe7bc939b9912c0247904800e0da386c7909
Jen Li <jen.li@duke.edu>
2019-12-08 05:19:20 -0500

    new releases on discover page

---
+++ src/public/discover.js
@@ -0,0 +1,30 @@
+var newReleasesTemplate = Handlebars.getTemplate('new-releases-template');
+document.addEventListener('DOMContentLoaded', async function() {
+    
+	var newReleasesPlaceholder = document.getElementById('newReleases');
+	//var newReleasesPlaceholder = this.newReleasesPlaceholder;
+
+//    var auth = await firebase.auth();
+//	var user = auth.currentUser;
+	console.log("HIII");
+//	console.log(user);
+//	var ref = firebase.database().ref(`/spotifyAccessToken/${user.uid}`)
+//    const snapshot = await ref.once('value');
+    var access_token = localStorage.getItem('access_token');
+
+	$.ajax({ // fill in users top tracks
+        url: 'https://api.spotify.com/v1/browse/new-releases',
+        headers: {
+          'Authorization': 'Bearer ' + access_token
+        },
+        success: function(response) {
+			console.log(response.albums);
+          newReleasesPlaceholder.innerHTML = newReleasesTemplate(response);
+        }
+    });
+
+
+  
+  });
+
+
