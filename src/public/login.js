cdfea915a05edf139609fe76d669600017a3b378
avivahwang <avivahwang@yahoo.com>
2019-12-08 15:39:37 -0500

    functioning chart code

--- src/public/login.js
+++ src/public/login.js
@@ -150,12 +150,43 @@ Demo.prototype.analyzePlaylists = function() {
 
   });
 
-    //document.getElementById("chart_acousticness").value = userPlaylistsAggFeatures.acousticness;
-    //document.getElementById("chart_danceability").value = userPlaylistsAggFeatures.danceability;
-    //document.getElementById("chart_energy").value = userPlaylistsAggFeatures.energy;
-    //document.getElementById("chart_instrumentalness").value = userPlaylistsAggFeatures.instrumentalness;
-    //document.getElementById("chart_speechiness").value = userPlaylistsAggFeatures.speechiness;
-    //document.getElementById("chart_valence").value = userPlaylistsAggFeatures.valence;
+    var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['acousticness', 'danceability', 'energy', 'instrumentalness', 'speechiness', 'valence'],
+                datasets: [{
+                    label: 'Average Playlist Qualities',
+                    data: [userPlaylistsAggFeatures.acousticness, userPlaylistsAggFeatures.danceability, userPlaylistsAggFeatures.energy, userPlaylistsAggFeatures.instrumentalness, userPlaylistsAggFeatures.speechiness, userPlaylistsAggFeatures.valence],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
 
 
   //TODO: fully parse playlists and generate visuals using chartjs
